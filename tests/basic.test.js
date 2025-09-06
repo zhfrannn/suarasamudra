f5fe122070d7f8a7a2a6af9f6ba74924aae30321
zhfrannn <hazhranz@gmail.com>
2025-09-06 16:49:57 +0700

    Update basic.test.js

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -2,5 +2,5 @@ const test = require('node:test');
 const assert = require('node:assert/strict');
 
 test('basic arithmetic works', () => {
-  assert.strictEqual(1  2, 3);
+  assert.strictEqual(1 + 2, 3);
 });
